61aae7e0b2fba5adebee2009574128f66fee70a0
lokilal <k_kirill45@mail.ru>
2022-08-06 23:24:57 +0300

    Отправка post запроса на создание нового пользователя

--- telegram_bot/app/handlers/customer/create.py
+++ telegram_bot/app/handlers/customer/create.py
@@ -1,5 +1,12 @@
 from aiogram import types
+import requests
 
 
 async def create_customer(call: types.CallbackQuery):
-    await call.message.answer('Привет обычным пользователям!')
+    context = {
+        'name': call.message.chat.username,
+        'telegram_id': call.message.chat.id
+    }
+    requests.post('http://127.0.0.1:8000/api/v1/customers/',
+                  data=context)
+    await call.message.answer(f'Добро пожаловать {context["name"]} в наше общество!')
